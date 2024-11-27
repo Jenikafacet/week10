34818e60a126c55de9b906a5ef23f279859395e5
Jenikafacet <scissors.cross@gmail.com>
2024-11-27 20:03:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Я учу JavaScript!");
+
+let image = document.getElementById('cat-img1');
+
+function changeImage1() {
+    image.src = "/assets/img/cat2.jpg"
+}
+
+function changeImage2() {
+    image.src = "/assets/img/cat1.jpg"
+}
